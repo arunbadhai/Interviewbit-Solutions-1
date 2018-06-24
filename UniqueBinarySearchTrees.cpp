08dda71b33881fbd4b51c8a1bd83deddb8759835
SanthoshBanavath <30873933+SanthoshBanavath@users.noreply.github.com>
2018-06-24 23:29:36 +0530

    Create UniqueBinarySearchTrees.cpp
    Solution for checkpoint level 7.

---
+++ UniqueBinarySearchTrees.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<TreeNode*> Getall(int start,int end){
+    vector<TreeNode*> sol;
+    if(start>end) {
+        sol.push_back(NULL);
+        return sol;
+    }
+    for(int i=start;i<=end;i++){
+        vector<TreeNode*> lefty=Getall(start,i-1);
+        vector<TreeNode*> righty=Getall(i+1,end);
+        for(TreeNode* left:lefty){
+            for(TreeNode* right:righty){
+                TreeNode* node=new TreeNode(i);
+                node->left=left;
+                node->right=right;
+                sol.push_back(node);
+            }
+        }
+    }
+    return sol;
+}
+
+vector<TreeNode*> Solution::generateTrees(int A) {
+    return Getall(1,A);//To recur all left and right subtrees
+}
