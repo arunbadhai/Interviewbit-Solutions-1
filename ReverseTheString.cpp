0eb04be7c042b7fabddc043f9ce4687d855e47f7
SanthoshBanavath <30873933+SanthoshBanavath@users.noreply.github.com>
2018-06-25 13:20:01 +0530

    Create ReverseTheString.cpp

---
+++ ReverseTheString.cpp
@@ -0,0 +1,29 @@
+void Solution::reverseWords(string &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    string st;
+    stack<int> s;
+    int n=A.length()-1;
+    int i=n;
+    while(A[i]==' ') i--;
+    while(i>=0){
+        if(A[i]==' '){
+            while(!s.empty()){
+                st.push_back(s.top());
+                s.pop();
+            }
+            st.push_back(' ');
+        }
+        else{
+            s.push(A[i]);
+        }
+        i--;
+    }
+    while(!s.empty()){
+        st.push_back(s.top());
+        s.pop();
+    }
+    A=st;
+}
